81d73c061d7d1f4f17e278c58c1b7b1aa24a3ddd
smilieke611@hotmail.com <smilieke611@hotmail.com@2195e172-cb09-ccdc-aed7-2c44663b7e48>
2012-05-07 13:43:16 +0000

    parser 1

--- RoboRally 3.0/src/roboRallyPackage/Parser.java
+++ RoboRally 3.0/src/roboRallyPackage/Parser.java
@@ -5,6 +5,12 @@ package roboRallyPackage;
 
 import static java.lang.System.out;
 import roboRallyPackage.commandClasses.*;
+import roboRallyPackage.commandClasses.BasicCommand.*;
+import roboRallyPackage.commandClasses.BooleanCommand.*;
+import roboRallyPackage.commandClasses.ConditionCommand.*;
+import roboRallyPackage.commandClasses.ControleStatementCommand.*;
+import roboRallyPackage.commandClasses.*;
+import roboRallyPackage.gameElementClasses.*;
 
 /**
  * @author Nele
@@ -12,208 +18,83 @@ import roboRallyPackage.commandClasses.*;
  */
 public class Parser
 {
-	public Command parse(String fullProgram)
+	Parser(Robot robot)
 	{
-		String[] words = fullProgram.split(" ");
-		
-		if(words.contains("while("))
-		{
-			
-		}
-		
-		
-		return null;
+		this.robot = robot;
 	}
 	
-	private void processCommand(String command) {
-		
-		if(words[0].equals("addrobot") && 4 <= words.length && words.length <= 5) {
-			String name = words[1];
-			if(robots.containsKey(name)) {
-				out.println("robot named " + name + " already exists");
-				return;
-			}
-			long x, y;
-			try {
-				x = Long.parseLong(words[2]);
-				y = Long.parseLong(words[3]);
-			} catch(NumberFormatException ex) {
-				out.println("position expected but found " + words[2] + " " + words[3]);
-				return;
-			}
-			double initialEnergy = 10000;
-			if(5 <= words.length ) {
-				try {
-					initialEnergy = Double.parseDouble(words[4]);
-				} catch(NumberFormatException ex) {
-					out.println("double expected but found " + words[4]);
-					return;
-				}
-			}
-			Robot newRobot = facade.createRobot(1, initialEnergy);
-			if(newRobot != null) {
-				robots.put(words[1], newRobot);
-				facade.putRobot(board, x, y, newRobot);
-			}
-		} else if(words[0].equals("addbattery") && 4 <= words.length && words.length <= 6) {
-			String name = words[1];
-			if(batteries.containsKey(name)) {
-				out.println("battery named " + name + " already exists");
-				return;
-			}
-			long x, y;
-			try {
-				x = Long.parseLong(words[2]);
-				y = Long.parseLong(words[3]);
-			} catch(NumberFormatException ex) {
-				out.println("position expected but found " + words[2] + " " + words[3]);
-				return;
-			}
-			double initialEnergy = 1000;
-			if(5 <= words.length ) {
-				try {
-					initialEnergy = Double.parseDouble(words[4]);
-				} catch(NumberFormatException ex) {
-					out.println("double expected but found " + words[4]);
-					return;
-				}
-			}
-			int weight = 1500;
-			if(6 <= words.length) {
-				try {
-					weight = Integer.parseInt(words[5]);
-				} catch(NumberFormatException ex) {
-					out.println("integer expected but found " + words[5]);
-					return;
-				}
-			}
-			Battery newBattery = facade.createBattery(initialEnergy, weight);
-			if(newBattery != null) {
-				batteries.put(words[1], newBattery);
-				facade.putBattery(board, x, y, newBattery);
-			}
-		} else if(words[0].equals("addwall") && words.length == 3) {
-			int x, y;
-			try {
-				x = Integer.parseInt(words[1]);
-				y = Integer.parseInt(words[2]);
-			} catch(NumberFormatException ex) {
-				out.println("position expected but found " + words[1] + " " + words[2]);
-				return;
-			}
-			Wall wall = facade.createWall();
-			if(wall != null) {
-				facade.putWall(board, x, y, wall);
-			}
-		} else if(words[0].equals("move") && words.length == 2) {
-			String name = words[1];
-			if(! robots.containsKey(name)) {
-				out.println("robot named " + name + " does not exist");
-				return;
-			}
-			facade.move(robots.get(name));
-		} else if(words[0].equals("turn") && words.length == 2) {
-			String name = words[1];
-			if(! robots.containsKey(name)) {
-				out.println("robot named " + name + " does not exist");
-				return;
-			}
-			facade.turn(robots.get(name));
-		} else if(words[0].equals("pickup") && words.length == 3) {
-			String rname = words[1];
-			if(! robots.containsKey(rname)) {
-				out.println("robot named " + rname + " does not exist");
-				return;
-			}
-			String iname = words[2];
-			if(! batteries.containsKey(iname)) {
-				out.println("battery named " + iname + " does not exist");
-				return;
-			}
-			facade.pickUp(robots.get(rname), batteries.get(iname));
-		} else if(words[0].equals("use") && words.length == 3) {
-			String rname = words[1];
-			if(! robots.containsKey(rname)) {
-				out.println("robot named " + rname + " does not exist");
-				return;
-			}
-			String iname = words[2];
-			if(! batteries.containsKey(iname)) {
-				out.println("battery named " + iname + " does not exist");
-				return;
-			}
-			facade.use(robots.get(rname), batteries.get(iname));
-		} else if(words[0].equals("drop") && words.length == 3) {
-			String rname = words[1];
-			if(! robots.containsKey(rname)) {
-				out.println("robot named " + rname + " does not exist");
-				return;
-			}
-			String iname = words[2];
-			if(! batteries.containsKey(iname)) {
-				out.println("battery named " + iname + " does not exist");
-				return;
-			}
-			facade.drop(robots.get(rname), batteries.get(iname));
-		} 
-		else if(words[0].equals("moveto") && words.length == 3) {
-			String rname = words[1];
-			if(! robots.containsKey(rname)) {
-				out.println("robot named " + rname + " does not exist");
-				return;
-			}
-			String rname2 = words[2];
-			if(! robots.containsKey(rname2)) {
-				out.println("robot named " + rname2 + " does not exist");
-				return;
-			}
-			facade.moveNextTo(robots.get(rname), robots.get(rname2));
-		} else if(words[0].equals("shoot") && words.length == 2) {
-			String rname = words[1];
-			if(! robots.containsKey(rname)) {
-				out.println("robot named " + rname + " does not exist");
-				return;
-			}
-			facade.shoot(robots.get(rname));
-		} else if(words[0].equals("canreach") && words.length == 4) {
-			String name = words[1];
-			if(! robots.containsKey(name)) {
-				out.println("robot named " + name + " does not exist");
-				return;
-			}
-			int x, y;
-			try {
-				x = Integer.parseInt(words[2]);
-				y = Integer.parseInt(words[3]);
-			} catch(NumberFormatException ex) {
-				out.println("position expected but found " + words[2] + " " + words[3]);
-				return;
-			}
-			
-			double required = facade.getMinimalCostToReach(robots.get(name), x, y);
-			if(required == -1) {
-				out.println("no (blocked by obstacles)");
-			} else if(required == -2) {
-				out.println("no (insufficient energy)");
-			} else {
-				out.println("yes (consuming " + required + " ws)");
-			}
-		} else if(words[0].equals("help") && words.length == 1)  {
-			out.println("commands:");
-			out.println("\taddbattery <bname> <long> <long> [<double>] [<int>]");
-			out.println("\taddwall <long> <long>");
-			out.println("\taddrobot <rname> <long> <long> [<double>]");
-			out.println("\tmove <rname>");
-			out.println("\tturn <rname>");
-			out.println("\tshoot <rname>");
-			out.println("\tpickup <rname> <bname>");
-			out.println("\tuse <rname> <bname>");
-			out.println("\tdrop <rname> <bname>");
-			out.println("\tcanreach <rname> <long> <long>");
-			out.println("\tmoveto <rname> <long> <long>");
-			out.println("\texit");
-		} else {
-			out.println("unknown command");
-		}
+	private Robot robot;
+	
+	public Robot getRobot()
+	{
+		return this.robot;
 	}
+
+	public Executable parse(String fullProgram)
+	{
+		fullProgram = fullProgram.trim().toLowerCase();
+		Executable resultCommand  = null;
+		
+		// BasicCommands
+		if(fullProgram.startsWith("(move)"))
+		{
+			resultCommand = new Move(this.getRobot());
+		}
+		if(fullProgram.startsWith("(turn"))
+		{
+			String subProgram = fullProgram.substring("(turn".length()).trim();
+			if(subProgram.startsWith("clockwise)"))
+			{
+				resultCommand = new Turn(this.getRobot(),Direction.CLOCKWISE);
+			}
+			if(subProgram.startsWith("counterclockwise)"))
+			{
+				resultCommand = new Turn(this.getRobot(),Direction.COUNTER_CLOCKWISE);
+			}
+		}
+		if(fullProgram.startsWith("(shoot)"))
+		{
+			resultCommand = new Shoot(this.getRobot());
+		}
+		if(fullProgram.startsWith("(pick-up-and-use)"))
+		{
+			resultCommand = new PickupAndUse(this.getRobot());
+		}
+		
+		// ControleStatementCommands
+		if(fullProgram.startsWith("(seq"))
+		{
+			java.util.List<Executable> commands = new java.util.ArrayList<Executable>();
+			String subProgram = fullProgram.substring("(seq".length()).trim();
+			
+			boolean stop = false;
+			int i = 0;
+			
+			while(! stop)
+			{
+				if(subProgram.startsWith(")"))
+				{
+					stop = true;
+				}
+				else
+				{
+					commands.add(this.parse(subProgram));
+				}
+			}
+				
+			resultCommand = new Sequence(this.getRobot(), commands);
+		}
+		if(fullProgram.startsWith("(while"))
+		{
+			resultCommand = new While();
+		}
+		if(fullProgram.startsWith("(if"))
+		{
+			resultCommand = new If();
+		}
+		
+		return resultCommand;
+	}
+	
+
 }
