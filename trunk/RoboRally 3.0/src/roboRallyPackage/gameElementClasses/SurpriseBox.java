d0a02201e65433f61c9f14ced8bb2778f2aa57c1
smilieke611@hotmail.com <smilieke611@hotmail.com@2195e172-cb09-ccdc-aed7-2c44663b7e48>
2012-05-10 14:22:44 +0000

    surprise box half af

--- trunk/RoboRally 3.0/src/roboRallyPackage/gameElementClasses/SurpriseBox.java
+++ trunk/RoboRally 3.0/src/roboRallyPackage/gameElementClasses/SurpriseBox.java
@@ -16,25 +16,99 @@ import roboRallyPackage.exceptionClasses.IllegalPositionException;
  * @author Nele & Jonas
  *
  */
-public class SurpriseBox extends Item {
-
-
+public class SurpriseBox extends Item
+{
 	/**
 	 * Initialize this new SurpriseBox with the given weight.
+	 * 
 	 * @param	weight
 	 * 			The weight for this new SurpriseBox in grams.
 	 */
 	public SurpriseBox(int weight)
 	{
 		super(weight);
+		this.addAllMethods();
 	}
+	
+	private java.lang.reflect.Method[] surprises;
+	
+	private void addMethod(java.lang.reflect.Method method)
+	{
+		java.lang.reflect.Method newSurprises[] = new java.lang.reflect.Method[surprises.length];
+		newSurprises[surprises.length] = method;
+		surprises = newSurprises;
+	}
+	
+	private void addAllMethods()
+	{
+		try
+		{
+			java.lang.reflect.Method explodeMethod = this.getClass().getMethod("explode");
+			this.addMethod(explodeMethod);
+
+			java.lang.reflect.Method teleportMethod = this.getClass().getMethod("teleport");
+			this.addMethod(teleportMethod);
+
+			java.lang.reflect.Method otherItemMethod = this.getClass().getMethod("otherItem");
+			this.addMethod(otherItemMethod);
+		}
+		catch(NoSuchMethodException exc)
+		{
+			
+		}
+	}
+	
+	public java.lang.reflect.Method[] getAllMethods()
+	{
+		return surprises;
+	}
+	
+	private void explode()
+	{
+		this.takeHit();
+	}
+	
+	private void teleport()
+	{
+		
+	}
+	
+	private void otherItem(Robot robot)
+	{
+		// create a random number between 0 and 3
+		Random generator = new Random();
+		int randomInt = generator.nextInt(3);
+
+		// add the new item to the possessions of the robot.
+		if(randomInt == 0)
+		{
+			Battery battery = new Battery(2500, 5);
+			battery.setPosition(robot.getPosition());
+			battery.setBoard(robot.getBoard());
+			robot.pickUp(battery);
+		}
+		if(randomInt == 1)
+		{
+			RepairKit repairKit = new RepairKit(500,5);
+			repairKit.setPosition(robot.getPosition());
+			repairKit.setBoard(robot.getBoard());
+			robot.pickUp(repairKit);
+		}
+		if(randomInt == 2)
+		{
+			SurpriseBox surpriseBox = new SurpriseBox(5);
+			surpriseBox.setPosition(robot.getPosition());
+			surpriseBox.setBoard(robot.getBoard());
+			robot.pickUp(surpriseBox);
+		}
+	}
+	
 
 	/**
 	 * A robot uses this SurpriseBox. One of the following three events will occur:
 	 * 1. The SurpriseBox explodes, hitting the robot and all other Elements on the position of this robot and its adjacent positions.
 	 * 2. The robot is teleported to another location.
-	 * 3. The SurpriseBox contained another Item (either a battery, a repairkit or 	 new surprisebox),
-	 * 		which will be added to the list of items carried by the robot
+	 * 3. The SurpriseBox contained another Item (either a battery, a repair kit or a new surprise box), which will be added to the list of items carried by the robot
 	 * 
 	 * @effect	...
 	 * 			| for one random integer i in 0..2:
@@ -49,8 +123,6 @@ public class SurpriseBox extends Item {
 	 * 			|			if( j == 1) than  robot.getPossessions().add(new SurpriseBox(5))
 	 * @effect	...
 	 * 			| this.terminate()
-	 * 						
-	 * 
 	 * @throws	IllegalStateException
 	 * 			...
 	 * 			| this.isTerminated() || robot.isTerminates()
@@ -67,65 +139,69 @@ public class SurpriseBox extends Item {
 		{
 			throw new IllegalStateException("A terminated robot can not be altered.");
 		}
-		// the robot has enough 'room' to store the total amount of energy this battery holds.
-
+		
+		// create a random number between 0 and 3
 		Random generator = new Random();
-		// Variable containing a random number between 0 and the number of elements on the checked position
-		int randomInt = generator.nextInt(3);
-
-		//the box explodes and the robot is hit.
-		if(randomInt == 0){
-			this.takeHit();
-		}
-		//the robot is teleported.
-		if(randomInt == 1){
-			//			Teleporter tele = new Teleporter(robot);
-			//			tele.teleport();
-			new Terminatable(){
-				public void teleport(){
-					Random generator =  new Random();
-					//long maxCoordX = board.getWidth();
-					//long maxCoordY = board.getHeight();
-
-					long maxCoordX = tempRobot.getBoard().getWidth();
-					long maxCoordY = tempRobot.getBoard().getHeight();
-
-
-
-					int randomIntCoordX = generator.nextInt(((int) maxCoordX));
-					int randomIntCoordY = generator.nextInt(((int) maxCoordY));
-					Position teleLocation = new Position(randomIntCoordX, randomIntCoordY);
-					try{
-						robot.setPosition(teleLocation);
-					} catch(IllegalPositionException exc){
-						robot.terminate();
-						System.out.println("Uw robot is nu dood. Hoe jammer!");
-					}
-
-				}
-			};
-
-		}
-		// a new random item is created
-		if(randomInt == 2){
-			int randomIntTwo = generator.nextInt(3);
-			if(randomIntTwo == 0){
-				Battery bat = new Battery(2500, 5);
-				robot.getPossessions().add(bat);
-			}
-
-
-			if(randomIntTwo == 1){
-				RepairKit kit = new RepairKit(500,5);
-				robot.getPossessions().add(kit);
-			}
-			if(randomIntTwo == 2){
-				SurpriseBox newBox = new SurpriseBox(5);
-				robot.getPossessions().add(newBox);
-			}
-		}
-
+		int randomInt = generator.nextInt(this.getAllMethods().length);
+		
+		// a random method is selected and invoked
+		this.getAllMethods()[randomInt].invoke(robot);
+		
 		this.terminate();
+
+//		//the box explodes and the robot is hit.
+//		if(randomInt == 0){
+//			this.takeHit();
+//		}
+//		//the robot is teleported.
+//		if(randomInt == 1){
+//			//			Teleporter tele = new Teleporter(robot);
+//			//			tele.teleport();
+//			new Terminatable(){
+//				public void teleport(){
+//					Random generator =  new Random();
+//					//long maxCoordX = board.getWidth();
+//					//long maxCoordY = board.getHeight();
+//
+//					long maxCoordX = tempRobot.getBoard().getWidth();
+//					long maxCoordY = tempRobot.getBoard().getHeight();
+//
+//
+//
+//					int randomIntCoordX = generator.nextInt(((int) maxCoordX));
+//					int randomIntCoordY = generator.nextInt(((int) maxCoordY));
+//					Position teleLocation = new Position(randomIntCoordX, randomIntCoordY);
+//					try{
+//						robot.setPosition(teleLocation);
+//					} catch(IllegalPositionException exc){
+//						robot.terminate();
+//						System.out.println("Uw robot is nu dood. Hoe jammer!");
+//					}
+//
+//				}
+//			};
+//
+//		}
+//		// a new random item is created
+//		if(randomInt == 2){
+//			int randomIntTwo = generator.nextInt(3);
+//			if(randomIntTwo == 0){
+//				Battery bat = new Battery(2500, 5);
+//				robot.getPossessions().add(bat);
+//			}
+//
+//
+//			if(randomIntTwo == 1){
+//				RepairKit kit = new RepairKit(500,5);
+//				robot.getPossessions().add(kit);
+//			}
+//			if(randomIntTwo == 2){
+//				SurpriseBox newBox = new SurpriseBox(5);
+//				robot.getPossessions().add(newBox);
+//			}
+//		}
+
+
 	}
 
 
