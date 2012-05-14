deabdd5a02f4bba6fa2dbd71f3a81b45acb9b6c3
schouterden_jonas@hotmail.com <schouterden_jonas@hotmail.com@2195e172-cb09-ccdc-aed7-2c44663b7e48>
2012-05-14 16:23:16 +0000

    gedeeltelijk nieuwe parser

--- trunk/RoboRally 3.0/src/roboRallyPackage/Parser.java
+++ trunk/RoboRally 3.0/src/roboRallyPackage/Parser.java
@@ -6,6 +6,7 @@ package roboRallyPackage;
 import static java.lang.System.out;
 
 import java.util.ArrayList;
+import java.util.regex.PatternSyntaxException;
 
 import roboRallyPackage.commandClasses.*;
 import roboRallyPackage.commandClasses.BasicCommand.*;
@@ -124,6 +125,78 @@ public class Parser
 		return resultCommand;
 	}
 	
+	
+	public java.util.ArrayList<Program> parse2(String inputProgram){
+		
+		inputProgram = inputProgram.replaceAll(" ","").toLowerCase();
+		java.util.ArrayList<String> progAsSubProgs = this.getSubstringsBracketCutter(inputProgram);
+		java.util.ArrayList<> program = new ArrayList<Command>();
+		
+		for(int i=0; i<=progAsSubProgs.size()-1;i++){
+		// BasicCommands
+				if(progAsSubProgs.get(i).equals("(move)"))
+				{
+					program.add(new Move(this.getRobot()));
+				}
+				if(progAsSubProgs.get(i).equals("(turnclockwise)"))
+				{
+					program.add( new Turn(this.getRobot(),Direction.CLOCKWISE));
+				}
+				if(progAsSubProgs.get(i).equals("(turncounterclockwise)"))
+				{
+					program.add( new Turn(this.getRobot(),Direction.COUNTER_CLOCKWISE));
+				}
+				if(progAsSubProgs.get(i).equals("(shoot)"))
+				{
+					program.add(new Shoot(this.getRobot()));
+				}
+				if(progAsSubProgs.get(i).equals("(pick-up-and-use)"));
+				{
+					program.add(new PickupAndUse(this.getRobot()));
+				}
+				
+				// ControleStatementCommands
+				if(progAsSubProgs.get(i).startsWith("(seq"))
+				{
+					
+					String subProgram = progAsSubProgs.get(i).replaceFirst("(seq", "").trim();
+					// het haakje ')' van "(seq" moet er nog vanachter afgeknipt worden
+					subProgram = subProgram.substring(0, subProgram.length()-2);
+					java.util.ArrayList subProgAsSubProgs=this.parse2(subProgram);
+					if(subProgAsSubProgs.contains(Condition.class)){
+						throw new IllegalSyntaxException ;
+					}
+					
+					program.add(new Sequence(this.getRobot(), ((java.util.ArrayList<Command>) subProgAsSubProgs)));
+					
+				}
+				if(progAsSubProgs.get(i).startsWith("(while"))
+				{
+					String subProgram = progAsSubProgs.get(i).replaceFirst("(while", "").trim();
+					subProgram = subProgram.substring(0, subProgram.length()-2);
+					java.util.ArrayList<Program> subProgAsSubProgs=this.parse2(subProgram);
+					
+					if(subProgAsSubProgs.size() == 2 
+							&& (subProgAsSubProgs.get(0) instanceof  Condition)
+							&& (subProgAsSubProgs.get(1) instanceof  Command)){
+					program.add(new While(this.getRobot(), (Condition) subProgAsSubProgs.get(0), (Command)subProgAsSubProgs.get(1)));
+					}
+				
+				}
+				if(progAsSubProgs.get(i).startsWith("(if"))
+				{
+					String subProgram = progAsSubProgs.get(i).replaceFirst("(if", "").trim();
+					subProgram = subProgram.substring(0, subProgram.length()-2);
+					java.util.ArrayList<Program> subProgAsSubProgs=this.parse2(subProgram);
+					
+					if(subProgAsSubProgs.size() == 3 && (subProgAsSubProgs.get(0) instanceof Condition) && (subProgAsSubProgs.get(1) instanceof Command )&&(subProgAsSubProgs.get(2) instanceof Command)){
+						program.add(new If(this.getRobot(), ((Condition) subProgAsSubProgs.get(0)), ((Command)subProgAsSubProgs.get(1)), ((Command )subProgAsSubProgs.get(2)));
+				} 
+				}
+				return program;
+			}
+	}
+	
 	public java.util.ArrayList<String> getSubstringsBracketCutter(String fullProgram){
 		// stores the index of the first '(' in the given String
 		int openingBracketIndex = 0;
@@ -135,13 +208,12 @@ public class Parser
 		// counts the number of closing brackets ')' the loop encounters 
 		// until the loop encounters the closing bracket ')' of the first opening bracket.
 		int closingBracketCounter = 0;
-		// variable representing the index of the array of characters, made by using the method .toCharArray() on the given String
-		int index = 0;
+	
 		
 		char[] fullPrStrings = fullProgram.toCharArray();
 		java.util.ArrayList<String> subStringList= new ArrayList<String>();
 
-		while(index <= fullPrStrings.length-1 ) {
+		for(int index=0; index <= fullPrStrings.length-1; index++) {
 			if(closingBracketCounter <= openingBracketCounter){
 
 				if (fullPrStrings[index] == '('){
@@ -156,10 +228,12 @@ public class Parser
 					}
 				}
 			}
+			else{
 			openingBracketCounter = 0;
 			closingBracketCounter = 0;
 
 			subStringList.add(fullProgram.substring(openingBracketIndex, closingBracketIndex));
+			}
 		}
 		return subStringList;
 	}
