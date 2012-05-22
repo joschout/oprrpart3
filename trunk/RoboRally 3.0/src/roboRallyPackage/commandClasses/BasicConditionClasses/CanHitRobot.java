a42d59e2213a1c6097d6cf78332f40adea7d4486
schouterden_jonas@hotmail.com <schouterden_jonas@hotmail.com@2195e172-cb09-ccdc-aed7-2c44663b7e48>
2012-05-22 10:03:04 +0000

    results (element)

--- trunk/RoboRally 3.0/src/roboRallyPackage/commandClasses/BasicConditionClasses/CanHitRobot.java
+++ trunk/RoboRally 3.0/src/roboRallyPackage/commandClasses/BasicConditionClasses/CanHitRobot.java
@@ -26,31 +26,9 @@ public class CanHitRobot extends BasicCondition
 	
 	public boolean results()
 	{
-		// get the first position in the line of the given robot that contains an element
-		Element testElement = this.getRobot().getBoard().getFirstElementStartingAt(this.getRobot().getPosition(), this.getRobot().getOrientation());
-		
-		// if some element is found it may be possible that more elements are placed on that position
-		// if one of them is a robot, this method should return true
-		if(testElement != null)
-		{
-			for(Element element: this.getRobot().getBoard().getElements(testElement.getPosition()))
-			{
-				if(element instanceof Robot)
-				{
-					return true;
-				}
-			}
-		}
-		return false;
+		return this.results(this.getRobot());
 	}
 	
-	@Override
-	public String toString()
-	{
-		return "(can-hit-robot)";
-	}
-
-
 	@Override
 	public boolean results(Element element) throws IllegalArgumentException {
 		if(!(element instanceof Robot)){
@@ -58,7 +36,7 @@ public class CanHitRobot extends BasicCondition
 		}
 		// get the first position in the line of the given robot that contains an element
 		Element testElement = element.getBoard().getFirstElementStartingAt(element.getPosition(), ((Robot)element).getOrientation());
-
+	
 		// if some element is found it may be possible that more elements are placed on that position
 		// if one of them is a robot, this method should return true
 		if(testElement != null)
@@ -72,5 +50,12 @@ public class CanHitRobot extends BasicCondition
 			}
 		}
 		return false;
+	}
+
+
+	@Override
+	public String toString()
+	{
+		return "(can-hit-robot)";
 	}
 }
--- trunk/RoboRally 3.0/src/roboRallyPackage/commandClasses/BasicConditionClasses/NextToWall.java
+++ trunk/RoboRally 3.0/src/roboRallyPackage/commandClasses/BasicConditionClasses/NextToWall.java
@@ -23,24 +23,47 @@ public class NextToWall extends BasicCondition
 		return this.robot;
 	}
 	
-	public boolean results()
-	{
-		for(Position position: this.getRobot().getPosition().getAllNeighbours())
-		{
-			for(Element element: this.getRobot().getBoard().getElements(position))
-			{
-				if(element instanceof Wall)
-				{
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+//	public boolean results()
+//	{
+//		for(Position position: this.getRobot().getPosition().getAllNeighbours())
+//		{
+//			for(Element element: this.getRobot().getBoard().getElements(position))
+//			{
+//				if(element instanceof Wall)
+//				{
+//					return true;
+//				}
+//			}
+//		}
+//		return false;
+//	}
 	
 	@Override
 	public String toString()
 	{
 		return "(wall)";
 	}
+
+	@Override
+	public boolean results() {
+		return this.results(this.getRobot());
+	}
+
+	@Override
+	public boolean results(Element element) throws IllegalArgumentException {
+		if(!(element instanceof Robot)){
+		throw new IllegalArgumentException();
+		}
+		
+		Position rightPosition = element.getPosition().getNeighbour(Orientation.RIGHT);
+		for(Element el: element.getBoard().getElements(rightPosition))
+			{
+				if(el instanceof Wall)
+				{
+					return true;
+				}
+			}
+		return false;
+	}
 }
+
